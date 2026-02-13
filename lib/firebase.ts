3c2fa748e3262fa94872267459f03c84e6a3ffdc
procatt182 <emoepinkleaf@gmail.com>
2026-02-13 17:46:29 +0200

    Remove Firebase configuration from firebase.ts
    Removed Firebase configuration object from the file.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -12,14 +12,6 @@ import { getFirestore } from "firebase/firestore";
 // Project Settings > General > Your apps > Firebase SDK snippet
 // ============================================================
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC0bZ2Kvw_5GS1VqFf6HuSl3BhEwrKtk8Q",
-  authDomain: "maikatisymebal.firebaseapp.com",
-  projectId: "maikatisymebal",
-  storageBucket: "maikatisymebal.firebasestorage.app",
-  messagingSenderId: "100684443502",
-  appId: "1:100684443502:web:f5cbf15c958af909a49b7f"
-};
 
 // Initialize Firebase (prevent re-initialization in hot-reload)
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
